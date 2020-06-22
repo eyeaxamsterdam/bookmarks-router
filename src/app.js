17ec0a69a0f544e2a9d834ee8e0df70f02499639
Dale <dallinpeacock@gmail.com>
2020-06-22 16:03:32 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandlers = require('./error-handlers')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
+
+const app = express()
+
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
+app.use(validateBearerToken)
+
+app.use(bookmarksRouter)
+
+app.get('/', (req, res) => {
+  res.send('Hello, express!')
+})
+
+app.use(errorHandlers)
+
+module.exports = app
